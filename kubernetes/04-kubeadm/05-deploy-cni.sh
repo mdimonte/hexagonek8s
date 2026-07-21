e55d1b96ece6015ff5e8ad31d7460378dc55c0db
mdimonte <mdimonte@calpeabyla.com>
2026-07-21 14:05:15 +0200

    feat(kubeadm): deploy CNI

--- kubernetes/04-kubeadm/05-deploy-cni.sh
+++ kubernetes/04-kubeadm/05-deploy-cni.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/v1_crd_projectcalico_org.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/tigera-operator.yaml
+
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/custom-resources-bpf.yaml -o - | kubectl apply -f -
+
+printf "check the status of the calico deployment using 'watch kubectl get tigerastatus'\n"
+printf "or 'watch kubectl get pods -A'\n\n"
